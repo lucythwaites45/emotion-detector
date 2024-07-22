778d5af3fb1e6cf6fb561c0fab5d91307d81b935
lucythwaites45 <89952016+lucythwaites45@users.noreply.github.com>
2024-07-22 21:32:07 +1200

    adding camera dependancies

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:general_waste_classifier/view/not_using/accounts_page.dart';
 import 'package:general_waste_classifier/view/not_using/recommendation.dart';
@@ -5,7 +6,14 @@ import 'package:general_waste_classifier/view/components/general.dart';
 import 'package:general_waste_classifier/view/components/home.dart';
 import 'package:general_waste_classifier/view/components/homepage.dart';
 import 'package:general_waste_classifier/view/components/profile.dart';
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+// Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+// Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
   runApp(const MyApp());
 }
 
--- lib/view/components/homepage.dart
+++ lib/view/components/homepage.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
 
+import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:general_waste_classifier/view/components/homepage.dart';
@@ -9,8 +10,12 @@ import 'package:general_waste_classifier/view/components/home.dart';
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+
+
   @override
   State<HomePage> createState() => _HomePageState();
+
+
 }
 
 class _HomePageState extends State<HomePage> {
@@ -155,6 +160,53 @@ class CardMoreInfo extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({
+    super.key,
+    required this.camera,
+  });
+
+  final CameraDescription camera;
+
+  @override
+  TakePictureScreenState createState() => TakePictureScreenState();
+}
+
+class TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    // To display the current output from the Camera,
+    // create a CameraController.
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.camera,
+      // Define the resolution to use.
+      ResolutionPreset.medium,
+    );
+
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Fill this out in the next steps.
+    return Container();
   }
 }
 
